70db73f69daabb340fd7c0cb96039099ce66b649
Lauan Guermandi <lauanguermandi@gmail.com>
2021-02-26 05:53:09 -0300

    feat: melhoria na abstração da api

---
+++ src/Financeiro.Application/Controllers/ApiControllerBase.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Financeiro.Domain.Entities;
+using System;
+using Financeiro.Domain.Dtos.PessoaDtos;
+
+namespace Financeiro.Application.Controllers
+{
+    public class ApiControllerBase : ControllerBase
+    {
+        public BadRequestObjectResult Failure(string message) {
+            return BadRequest(new { error = message });
+        }
+
+        public ObjectResult ServerError(string message) {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new { error = message });
+        }
+
+        public OkObjectResult Success([ActionResultObjectValue] object value){
+            return Ok(value);
+        }
+
+        public Uri makeUri(PessoaDto entity, string routeName = null) 
+        {   
+            routeName = String.IsNullOrEmpty(routeName) ? "GetById" : routeName;
+            return new Uri(Url.Link(routeName, new { id = entity.Id }));
+        }
+    }
+}
---
+++ src/Financeiro.Application/Controllers/PessoaController.cs
@@ -0,0 +1,92 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Financeiro.Domain.Dtos.PessoaDtos;
+using Financeiro.Domain.Interfaces.Service;
+using Microsoft.AspNetCore.Mvc;
+using Financeiro.Domain.Exceptions;
+
+namespace Financeiro.Application.Controllers
+{
+    [Route("/api/v1/[controller]")]
+    [ApiController]
+    public class PessoaController : ApiControllerBase
+    {
+        private IPessoaService _service;
+
+        public PessoaController(IPessoaService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+                
+            try
+            {
+                var pessoas = await _service.GetAll();
+                if (pessoas == null) {
+                    return NotFound();
+                }
+                
+                return Success(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return ServerError(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetById")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var pessoa = await _service.Get(id);
+                if (pessoa == null) {
+                    return NotFound();
+                }
+
+                return Success(pessoa);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PessoaCreateDto pessoa)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = await this._service.Post(pessoa);
+                if (result != null)
+                {
+                    return Created(makeUri(result), result);
+                }
+
+                return BadRequest();
+            }
+            catch (DuplicatedValueException d)
+            {
+                return Failure(d.Message);
+            }
+            catch (ArgumentException e)
+            {
+                return ServerError(e.Message);
+            }
+        }
+    }
+}
