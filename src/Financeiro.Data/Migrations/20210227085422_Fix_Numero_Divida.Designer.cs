f356039017f512bfa62a4bc345d33e21e702a67f
Lauan Guermandi <lauanguermandi@gmail.com>
2021-02-27 06:07:08 -0300

    refactor: melhorias no código

--- src/Financeiro.Data/Migrations/20210227085422_Fix_Numero_Divida.Designer.cs
+++ src/Financeiro.Data/Migrations/20210227085422_Fix_Numero_Divida.Designer.cs
@@ -2,14 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210227085422_Fix_Numero_Divida")]
+    partial class Fix_Numero_Divida
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Financeiro.Data/Migrations/20210227085422_Fix_Numero_Divida.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class Fix_Numero_Divida : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Divida_NumeroTitulo",
+                table: "Divida",
+                column: "NumeroTitulo",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Divida_NumeroTitulo",
+                table: "Divida");
+        }
+    }
+}
