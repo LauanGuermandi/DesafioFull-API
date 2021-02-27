f2c3f6a6e7a2eabc29aff3ff761fdbcbf04cc34c
Lauan Guermandi <lauanguermandi@gmail.com>
2021-02-26 23:59:12 -0300

    feat: melhoria no padrão repository e adição da listagem de dividas

--- src/Financeiro.Domain/Dtos/DividaDtos/DividaResultDto.cs
+++ src/Financeiro.Domain/Dtos/DividaDtos/DividaResultDto.cs
@@ -3,7 +3,7 @@ using System;
 namespace Financeiro.Domain.Dtos.DividaDtos
 {
     public class DividaResultDto
-    {        
+    {
         public Guid Id { get; set; }
         public double ValorOriginal { get; set; }
         public string NumeroTitulo { get; set; }
