f33c26174115a51977859e344624b91f128b3527
Lauan Guermandi <lauanguermandi@gmail.com>
2021-02-27 01:46:48 -0300

    refactor: remoção dos parametros

--- src/Financeiro.Data/Context/DataContext.cs
+++ src/Financeiro.Data/Context/DataContext.cs
@@ -2,23 +2,18 @@ using Financeiro.Data.Mappings;
 using Financeiro.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-public class DataContext : DbContext
-{
+public class DataContext : DbContext {
     public DbSet<Pessoa> Pessoas { get; set; }
     public DbSet<Divida> Dividas { get; set; }
     public DbSet<Parcela> Parcelas { get; set; }
-    public DbSet<Parametro> Parametros { get; set; }
 
-    public DataContext(DbContextOptions options) : base(options)
-    {
+    public DataContext(DbContextOptions options) : base(options) {
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new PessoaMap());
         modelBuilder.ApplyConfiguration(new DividaMap());
         modelBuilder.ApplyConfiguration(new ParcelaMap());
-        modelBuilder.ApplyConfiguration(new ParametroMap());
     }
 }
--- src/Financeiro.Domain/Parameters/ParametroFinanceiro.cs
+++
@@ -1,8 +0,0 @@
-namespace Financeiro.Domain.Parameters
-{
-    enum ParametroFinanceiro
-    {
-        MULTA,
-        JUROS
-    }
-}
